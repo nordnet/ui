b4d38af152167e958d108ec5c2581f8a2e444312
Mansour J. Mansour <mansour.mansour@nordnet.se>
2019-03-20 19:46:05 +0100

    feat: add initial component

---
+++ src/Organisms/Grid/Grid.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Grid from '.';
+
+const stories = storiesOf('Organisms/Grid', module);
+
+stories.add('CSS Grid', () => <Grid />);
---
+++ src/Organisms/Grid/Grid.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Grid = styled.div`
+  box-sizing: border-box;
+  display: grid;
+`;
