22004949555d18d89aecc8bda6c51d8866a72fbc
Ivan Shmidt <bahoc47@gmail.com>
2019-03-27 14:20:56 +0100

    fix: jest config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import Media from './Atoms/Media';
 import Grid from './Atoms/Grid';
+import Text from './Atoms/Text';
+
 import theme from './theme';
+
 import { Theme } from './theme/theme.types';
 
 /** Runtime code */
-export { theme, Media, Grid };
+export { theme, Media, Grid, Text };
 
 /** Type definitions */
 export { Theme };
