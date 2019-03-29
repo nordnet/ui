dc35f89bbbf1248d61d7d569c5c766ecd94dd1fd
Max Åberg <aaberg.max@gmail.comgit>
2019-03-29 13:35:56 +0100

    feat: created contract for Link

---
+++ src/Molecules/Link/Link.types.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LinkProps } from 'react-router-dom';
+
+export type Props = {
+  disabled?: boolean;
+  onClick?: (e: React.MouseEvent) => void;
+  to: LinkProps['to'];
+  /** @default _self */
+  target?: '_blank';
+};
+
+export type LinkComponent = React.FunctionComponent<Props>;
