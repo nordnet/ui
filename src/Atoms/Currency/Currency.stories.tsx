371120deec8566b437c2a72bfe6fff64775eff19
Jacob Blomgren <blomgren@protonmail.com>
2019-03-29 15:25:34 +0100

    test: update snapshots

--- src/Atoms/Currency/Currency.stories.tsx
+++ src/Atoms/Currency/Currency.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, number, select } from '@storybook/addon-knobs';
 import Currency from './index';
