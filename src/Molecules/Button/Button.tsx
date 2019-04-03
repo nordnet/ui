87e26f00b8573ad7574a5b93c43526bc80b2db60
Mansour J. Mansour <mansour@mjmonline.se>
2019-04-03 19:23:05 +0200

    fix: remove comments

--- src/Molecules/Button/Button.tsx
+++ src/Molecules/Button/Button.tsx
@@ -50,45 +50,35 @@ const getHeight = (props: ThemedStyledProps<Props, Theme>) => {
   return `${theme.spacing.unit(hugeness)}px`;
 };
 
+/** From Normalize.css v8.0.1 */
 const NormalizedButton = styled.button`
-  /**
-   * From Normalize.css v8.0.1
-   * 1. Change the font styles in all browsers.
-   * 2. Remove the margin in Firefox and Safari.
-   * 3. Show the overflow in Edge.
-   * 4. Remove the inheritance of text transform in Edge, Firefox, and IE
-   * 5. Correct the inability to style clickable types in iOS and Safari.
-   * 6. Remove the inner border and padding in Firefox.
-   * 7. Restore the focus styles unset by the previous rule
-   */
-
-  font-family: inherit; /* 1 */
-  font-size: 100%; /* 1 */
-  line-height: 1.15; /* 1 */
-  margin: 0; /* 2 */
-  overflow: visible; /* 3 */
-  text-transform: none; /* 4 */
+  font-family: inherit;
+  font-size: 100%;
+  line-height: 1.15;
+  margin: 0;
+  overflow: visible;
+  text-transform: none;
 
   &,
   &[type='button'],
   &[type='reset'],
   &[type='submit'] {
-    -webkit-appearance: button; /* 5 */ /* stylelint-disable-line */
+    -webkit-appearance: button; /* stylelint-disable-line */
   }
 
   &::-moz-focus-inner,
   &[type='button']::-moz-focus-inner,
   &[type='reset']::-moz-focus-inner,
   &[type='submit']::-moz-focus-inner {
-    border-style: none; /* 6 */
-    padding: 0; /* 6 */
+    border-style: none;
+    padding: 0;
   }
 
   &:-moz-focusring,
   &[type='button']:-moz-focusring,
   &[type='reset']:-moz-focusring,
   &[type='submit']:-moz-focusring {
-    outline: 1px dotted ButtonText; /* 7 */
+    outline: 1px dotted ButtonText;
   }
 `;
 
