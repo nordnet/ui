40eae9fb94c3957eda770feb03d354b2e87e3a12
Mansour J. Mansour <mansour@mjmonline.se>
2019-04-03 15:05:54 +0200

    feat: normalize button

--- src/Molecules/Button/Button.tsx
+++ src/Molecules/Button/Button.tsx
@@ -50,7 +50,49 @@ const getHeight = (props: ThemedStyledProps<Props, Theme>) => {
   return `${theme.spacing.unit(hugeness)}px`;
 };
 
-const StyledButton = styled.button<Props>`
+const StyledNeutralButton = styled.button`
+  /**
+   * From Normalize.css v8.0.1
+   * 1. Change the font styles in all browsers.
+   * 2. Remove the margin in Firefox and Safari.
+   * 3. Show the overflow in Edge.
+   * 4. Remove the inheritance of text transform in Edge, Firefox, and IE
+   * 5. Correct the inability to style clickable types in iOS and Safari.
+   * 6. Remove the inner border and padding in Firefox.
+   * 7. Restore the focus styles unset by the previous rule
+   */
+
+  font-family: inherit; /* 1 */
+  font-size: 100%; /* 1 */
+  line-height: 1.15; /* 1 */
+  margin: 0; /* 2 */
+  overflow: visible; /* 3 */
+  text-transform: none; /* 4 */
+
+  &,
+  &[type='button'],
+  &[type='reset'],
+  &[type='submit'] {
+    -webkit-appearance: button; /* 5 */ /* stylelint-disable-line */
+  }
+
+  &::-moz-focus-inner,
+  &[type='button']::-moz-focus-inner,
+  &[type='reset']::-moz-focus-inner,
+  &[type='submit']::-moz-focus-inner {
+    border-style: none; /* 6 */
+    padding: 0; /* 6 */
+  }
+
+  &:-moz-focusring,
+  &[type='button']:-moz-focusring,
+  &[type='reset']:-moz-focusring,
+  &[type='submit']:-moz-focusring {
+    outline: 1px dotted ButtonText; /* 7 */
+  }
+`;
+
+const StyledButton = styled(StyledNeutralButton)<Props>`
   ${p => getBackgroundColor(p)}
   border-radius: 0;
   border: 2px solid ${p => (isSecondary(p.variant) ? p.theme.color.cta : 'transparent')};
@@ -73,3 +115,7 @@ export const Button: ButtonComponent = props => (
     {props.children}
   </StyledButton>
 );
+
+Button.defaultProps = {
+  type: 'button',
+};
