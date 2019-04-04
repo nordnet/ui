284c8372b03e551c7d1a9f7a5e6128b12b0127f3
Mansour J. Mansour <mansour@mjmonline.se>
2019-04-04 11:02:45 +0200

    fix: move normalized button to common

--- src/Molecules/Button/Button.tsx
+++ src/Molecules/Button/Button.tsx
@@ -4,6 +4,7 @@ import Color from 'color';
 import { ButtonComponent, Props } from './Button.types';
 import { Theme } from '../../theme/theme.types';
 import { isUndefined } from '../../common/utils';
+import NormalizedElements from '../../common/NormalizedElements';
 
 const HEIGHT = {
   s: 6,
@@ -50,39 +51,7 @@ const getHeight = (props: ThemedStyledProps<Props, Theme>) => {
   return `${theme.spacing.unit(hugeness)}px`;
 };
 
-/** From Normalize.css v8.0.1 */
-const NormalizedButton = styled.button`
-  font-family: inherit;
-  font-size: 100%;
-  line-height: 1.15;
-  margin: 0;
-  overflow: visible;
-  text-transform: none;
-
-  &,
-  &[type='button'],
-  &[type='reset'],
-  &[type='submit'] {
-    -webkit-appearance: button; /* stylelint-disable-line */
-  }
-
-  &::-moz-focus-inner,
-  &[type='button']::-moz-focus-inner,
-  &[type='reset']::-moz-focus-inner,
-  &[type='submit']::-moz-focus-inner {
-    border-style: none;
-    padding: 0;
-  }
-
-  &:-moz-focusring,
-  &[type='button']:-moz-focusring,
-  &[type='reset']:-moz-focusring,
-  &[type='submit']:-moz-focusring {
-    outline: 1px dotted ButtonText;
-  }
-`;
-
-const StyledButton = styled(NormalizedButton)<Props>`
+const StyledButton = styled(NormalizedElements.Button)<Props>`
   ${p => getBackgroundColor(p)}
   border-radius: 0;
   border: 2px solid ${p => (isSecondary(p.variant) ? p.theme.color.cta : 'transparent')};
