44293cada3b1a236b813e8275480efec66af00d8
Max Åberg <aaberg.max@gmail.comgit>
2019-04-08 18:24:18 +0200

    feat: created a titled card

---
+++ src/Molecules/TitledCard/TitledCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Text, Card } from '../..';
+import { TitledCardComponent, Props } from './TitledCard.types';
+
+const StyledTitle = styled.div<Props>`
+  margin: ${({ theme, title }) => theme.spacing.unit(5)}px;
+`;
+
+export const TitledCard: TitledCardComponent = props =>
+  props.title ? (
+    <Card>
+      <StyledTitle>
+        <Text.Title3>{props.title}</Text.Title3>
+      </StyledTitle>
+      {props.children}
+    </Card>
+  ) : (
+    <Card>{props.children}</Card>
+  );
+TitledCard.displayName = 'TitledCard';
