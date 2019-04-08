eb7e4061f311b2d009122bda3bfa95f90fc6a4d4
Max Åberg <aaberg.max@gmail.comgit>
2019-04-08 15:59:58 +0200

    feat: created Flag component

---
+++ src/Atoms/Flag/Flag.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+// @ts-ignore
+import flags from './flags';
+import { FlagComponent, Props } from './Flag.types';
+
+const StyledFlag = styled.svg<Props>`
+  width: ${p => p.theme.spacing.unit(p.width || 5)}px;
+  height: ${p => p.theme.spacing.unit(p.height || 5)}px;
+`;
+
+export const Flag: FlagComponent = props => {
+  const Component = flags[props.country.toLowerCase()];
+
+  return Component ? <StyledFlag as={Component} width={props.width} height={props.height} /> : null;
+};
