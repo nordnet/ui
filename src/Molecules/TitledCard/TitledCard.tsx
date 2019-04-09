4409350f84751ae2f663c172a6263cec6eeaa19a
Max Åberg <aaberg.max@gmail.comgit>
2019-04-09 10:20:29 +0200

    feat: omitted some props in the impl

--- src/Molecules/TitledCard/TitledCard.tsx
+++ src/Molecules/TitledCard/TitledCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import R from 'ramda';
 import { Text, Card } from '../..';
 import { TitledCardComponent, Props } from './TitledCard.types';
 
@@ -7,15 +8,17 @@ const StyledTitle = styled.div<Props>`
   margin: ${({ theme }) => theme.spacing.unit(5)}px;
 `;
 
+const omitProps = R.omit(['children', 'title']);
+
 export const TitledCard: TitledCardComponent = props =>
   props.title ? (
-    <Card>
+    <Card {...omitProps(props)}>
       <StyledTitle>
         <Text.Title3>{props.title}</Text.Title3>
       </StyledTitle>
       {props.children}
     </Card>
   ) : (
-    <Card>{props.children}</Card>
+    <Card {...omitProps(props)}>{props.children}</Card>
   );
 TitledCard.displayName = 'TitledCard';
