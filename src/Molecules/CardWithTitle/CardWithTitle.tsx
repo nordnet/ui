4467796a870ef5061aae864240250a41cd19b845
Max Åberg <aaberg.max@gmail.comgit>
2019-04-23 12:03:06 +0200

    feat: removed padding around card

--- src/Molecules/CardWithTitle/CardWithTitle.tsx
+++ src/Molecules/CardWithTitle/CardWithTitle.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
-import R from 'ramda';
 import { Typography, Card } from '../..';
 import { CardWithTitleComponent, Props } from './CardWithTitle.types';
 
-const StyledCard = styled(Card)`
+const StyledTitle = styled.div<Props>`
   padding: ${({ theme }) => theme.spacing.unit(5)}px;
 `;
-const StyledTitle = styled.div<Props>`
-  padding-bottom: ${({ theme }) => theme.spacing.unit(5)}px;
-`;
-
-const omitProps = R.omit(['children', 'title']);
 
 export const CardWithTitle: CardWithTitleComponent = props =>
   props.title ? (
-    <StyledCard {...omitProps(props)}>
+    <Card>
       <StyledTitle>
         <Typography type="title3">{props.title}</Typography>
       </StyledTitle>
       {props.children}
-    </StyledCard>
+    </Card>
   ) : (
-    <StyledCard {...omitProps(props)}>{props.children}</StyledCard>
+    <Card>{props.children}</Card>
   );
 CardWithTitle.displayName = 'CardWithTitle';
