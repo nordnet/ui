b9fbfaf087a3b0500869acf255cf8e98fe3f69d0
Max Åberg <aaberg.max@gmail.comgit>
2019-04-24 10:29:17 +0200

    feat: stories for right title

--- src/Molecules/CardWithTitle/CardWithTitle.stories.tsx
+++ src/Molecules/CardWithTitle/CardWithTitle.stories.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import styled from 'styled-components';
+import { HashRouter } from 'react-router-dom';
 
-import { CardWithTitle } from '../..';
+import { CardWithTitle, Link, Icon, Typography } from '../..';
+
+const PaddedIcon = styled(Icon.ArrowRight)`
+  padding-left: ${p => p.theme.spacing.unit(1)}px;
+`;
+
+const rightTitle = (
+  <HashRouter>
+    <Link to="www.google.com">
+      <Typography type="secondary" color={t => t.color.text} weight="bold">
+        Marknadsöversikt
+      </Typography>
+      <PaddedIcon color={t => t.color.cta} size={3} />
+    </Link>
+  </HashRouter>
+);
 
 storiesOf('Molecules | CardWithTitle', module)
   .add('Basic CardWithTitle', () => (
     <CardWithTitle title="Konton">A CardWithTitle containing content</CardWithTitle>
+  ))
+  .add('CardWithTitle with right title', () => (
+    <CardWithTitle title="Konton" rightTitle={rightTitle}>
+      A CardWithTitles containing content
+    </CardWithTitle>
   ))
   .add('CardWithTitle with no title', () => (
     <CardWithTitle>A CardWithTitle without title</CardWithTitle>
