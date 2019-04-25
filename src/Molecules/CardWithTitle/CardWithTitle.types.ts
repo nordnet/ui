409b4fe932cd104a0cc526e99613bde201ec9404
Max Åberg <aaberg.max@gmail.comgit>
2019-04-25 11:12:23 +0200

    feat: removed rightitle type

--- src/Molecules/CardWithTitle/CardWithTitle.types.ts
+++ src/Molecules/CardWithTitle/CardWithTitle.types.ts
@@ -2,7 +2,6 @@ import { Props as CardProps } from '../../Atoms/Card/Card.types';
 
 export type Props = {
   title?: React.ReactNode;
-  rightTitle?: React.ReactNode;
 };
 
 export type CardWithTitleComponent = React.FunctionComponent<CardProps & Props>;
