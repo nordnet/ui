9dd163d0b3353e55e791f20d44682ad07d8bed8c
Max Åberg <aaberg.max@gmail.comgit>
2019-04-25 11:02:01 +0200

    feat: revert back to one title

--- src/Molecules/CardWithTitle/CardWithTitle.tsx
+++ src/Molecules/CardWithTitle/CardWithTitle.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import R from 'ramda';
-import { Typography, Card, Flexbox } from '../..';
+import { Typography, Card } from '../..';
 import { CardWithTitleComponent, Props } from './CardWithTitle.types';
 
 const StyledTitle = styled.div<Props>`
-  padding: ${({ theme }) => theme.spacing.unit(4)}px;
+  margin: ${({ theme }) => theme.spacing.unit(5)}px;
 `;
 
 const omitProps = R.omit(['children', 'title']);
@@ -14,12 +14,7 @@ export const CardWithTitle: CardWithTitleComponent = props =>
   props.title ? (
     <Card {...omitProps(props)}>
       <StyledTitle>
-        <Flexbox.Container justifyContent="space-between">
-          <Flexbox.Item>
-            <Typography type="title3">{props.title}</Typography>
-          </Flexbox.Item>
-          <Flexbox.Item>{props.rightTitle ? props.rightTitle : null}</Flexbox.Item>
-        </Flexbox.Container>
+        <Typography type="title3">{props.title}</Typography>
       </StyledTitle>
       {props.children}
     </Card>
