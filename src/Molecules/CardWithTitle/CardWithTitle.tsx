94105e5eb3902c5d43012e01c56db4da233e5f21
Ivan Shmidt <bahoc47@gmail.com>
2019-04-26 13:56:43 +0200

    feat: 🎸 cardWithTitle simplification
    BREAKING CHANGE: 🧨 remove default styles from CardWithTitle

--- src/Molecules/CardWithTitle/CardWithTitle.tsx
+++ src/Molecules/CardWithTitle/CardWithTitle.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import R from 'ramda';
-import { Card, Typography } from '../..';
+import { Card } from '../..';
 import { CardWithTitleComponent } from './CardWithTitle.types';
 
 const StyledHeader = styled.header`
   padding: ${({ theme }) => theme.spacing.unit(5)}px;
 `;
 
-const isString = (x: any): x is string => typeof x === 'string' || x instanceof String;
-
 const omitProps = R.omit(['children', 'title']);
 
 export const CardWithTitle: CardWithTitleComponent = props => {
   const { title, children } = props;
-
-  if (isString(title)) {
-    return (
-      <Card {...omitProps(props)}>
-        <StyledHeader>
-          <Typography type="title3" as="h2">
-            {title}
-          </Typography>
-        </StyledHeader>
-        {children}
-      </Card>
-    );
-  }
 
   return (
     <Card {...omitProps(props)}>
