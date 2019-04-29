46e3a3139335eea63700b3076e4aa8e4ea81e9d7
Mansour J. Mansour <mansour@mjmonline.se>
2019-04-29 16:31:18 +0200

    feat: add pagewrapper component

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,35 @@
 /** Atoms */
+import { Table, Tbody, Td, Tfoot, Th, Thead, Tr } from './Atoms/Table';
 import Card from './Atoms/Card';
 import CssGrid from './Atoms/CssGrid';
-import List from './Atoms/List';
-import ListItem from './Atoms/ListItem';
 import Flag from './Atoms/Flag';
 import Flexbox from './Atoms/Flexbox';
 import Icon from './Atoms/Icon';
+import List from './Atoms/List';
+import ListItem from './Atoms/ListItem';
 import Media from './Atoms/Media';
+import Number from './Atoms/Number';
+import PageWrapper from './Atoms/PageWrapper';
 import Separator from './Atoms/Separator';
 import Spinner from './Atoms/Spinner';
-import { Table, Tbody, Td, Tfoot, Th, Thead, Tr } from './Atoms/Table';
-import Typography from './Atoms/Typography';
 import TabTitle from './Atoms/TabTitle';
-import Number from './Atoms/Number';
 import Time from './Atoms/Time';
+import Typography from './Atoms/Typography';
 
 /** Molecules */
+import Avatar from './Molecules/Avatar';
+import Button from './Molecules/Button';
+import CardWithTitle from './Molecules/CardWithTitle';
 import Currency from './Molecules/Currency/index';
 import Development from './Molecules/Development/index';
-import Tabs from './Molecules/Tabs';
-import TabsNav from './Molecules/TabsNav';
 import LabeledValue from './Molecules/LabeledValue';
 import Link from './Molecules/Link';
 import LinkBuy from './Molecules/LinkBuy';
 import LinkSell from './Molecules/LinkSell';
-import CardWithTitle from './Molecules/CardWithTitle';
-import Avatar from './Molecules/Avatar';
-import Button from './Molecules/Button';
-import Percent from './Molecules/Percent';
 import ListWithTitles from './Molecules/ListWithTitles';
+import Percent from './Molecules/Percent';
+import Tabs from './Molecules/Tabs';
+import TabsNav from './Molecules/TabsNav';
 
 /** Organisms */
 import CardWithTabs from './Organisms/CardWithTabs';
@@ -39,6 +40,7 @@ export {
   Avatar,
   Button,
   Card,
+  CardWithTabs,
   CardWithTitle,
   CssGrid,
   Currency,
@@ -50,26 +52,26 @@ export {
   Link,
   LinkBuy,
   LinkSell,
+  List,
+  ListItem,
   ListWithTitles,
   Media,
   Number,
+  PageWrapper,
   Percent,
   Separator,
   Spinner,
-  Time,
-  theme,
   Table,
+  Tabs,
+  TabsNav,
+  TabTitle,
   Tbody,
   Td,
   Tfoot,
   Th,
   Thead,
+  theme,
+  Time,
   Tr,
   Typography,
-  List,
-  ListItem,
-  Tabs,
-  TabsNav,
-  TabTitle,
-  CardWithTabs,
 };
