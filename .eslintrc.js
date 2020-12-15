1419bfbfc2106e221090cbb8f54f6ee149fdd354
Wiktoria Tomzik <wiktoria.tomzik@nordnet.se>
2020-12-15 12:45:15 +0100

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,13 +41,12 @@ module.exports = {
         ignoreRestSiblings: false,
       },
     ],
-    // remove this rule when Link component only render links
     'jsx-a11y/anchor-is-valid': [
+      // remove this rule when Link component only render links
       'error',
       {
         components: ['Link'],
         specialLink: ['to'],
-        aspects: [],
       },
     ],
   },
