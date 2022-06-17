9498164ec3984bd1c563e2448f9f2856e754cc05
william bruce <william.bruce@nordnet.se>
2022-06-17 16:10:17 +0200

    feat: add growth16 icon

---
+++ src/Atoms/Icon/finance/growth/Growth16.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { IconBase } from '../../IconBase';
+import { IconProps } from '../../IconBase.types';
+
+const Growth24: React.FC<IconProps> = (props) => {
+  return (
+    <IconBase {...props} width={16} height={16}>
+      <path
+        d="M10 3V5.82843L12 3.82843V16H14V3.82843L16 5.82843V3L13 0L10 3Z M8 7H6V16H8V7Z M0 11H2V16H0V11Z"
+        fill="currentColor"
+      />
+    </IconBase>
+  );
+};
+
+Growth24.displayName = 'Icon.Growth';
+
+export default Growth24;
