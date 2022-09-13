e95cf68c9a94c0415c1d65acda6b7967f3acfcd9
william bruce <william.bruce@nordnet.se>
2022-09-13 14:53:45 +0200

    docs: improve type documentation

--- src/Atoms/Flag/Flag.tsx
+++ src/Atoms/Flag/Flag.tsx
@@ -3,6 +3,17 @@ import { FlagComponent } from './Flag.types';
 import { assert } from '../../common/utils';
 import { FlagBase } from './components/FlagBase';
 
+/**
+ *
+ * @param {string} [className] - For styled-components support
+ * @param {string} [title] - Used for title tag in SVG. Will fall back to english country name
+ * @param {string} country - Country code
+ * @param {boolean} [inline] - Will give the svg display: inline-block and vertical-align: sub
+ * @param {string} size - The diameter of the flag
+ * - "s" = 14px
+ * - "m" = 16px
+ * - "l" = 24px
+ */
 export const Flag: FlagComponent = ({ size, country, ...rest }) => {
   assert(Boolean(country), 'Flag: You need to supply a country code');
   switch (size) {
--- src/Atoms/Flag/Flag.types.ts
+++ src/Atoms/Flag/Flag.types.ts
@@ -1,16 +1,31 @@
 import React from 'react';
 
-/**
- * 's' = 14px,
- * 'm' = 16px,
- * 'l' = 24px
- */
-type FlagSize = { size: 's' | 'm' | 'l' };
+type FlagSize = {
+  /**
+   * @param {string} size The diameter of the flag
+   * - "s" = 14px
+   * - "m" = 16px
+   * - "l" = 24px
+   */
+  size: 's' | 'm' | 'l';
+};
 
 export type PropsWithSize = {
+  /**
+   * @param {string} [className] For styled-components support
+   */
   className?: string;
+  /**
+   * @param {string} [title] Used for title tag in SVG. Will fall back to english country name
+   */
   title?: string;
+  /**
+   * @param {string} country Country code
+   */
   country: string;
+  /**
+   * @param {boolean} [inline] Will give the svg display: inline-block and vertical-align: sub
+   */
   inline?: boolean;
 } & FlagSize;
 
