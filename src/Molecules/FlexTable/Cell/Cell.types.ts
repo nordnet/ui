7a1f1b57f54435cadb7ae876c4c200aaf370a11a
Mansour Mansour <mansour@mjmonline.se>
2022-11-10 15:28:03 +0100

    fix: update flextable cell types

--- src/Molecules/FlexTable/Cell/Cell.types.ts
+++ src/Molecules/FlexTable/Cell/Cell.types.ts
@@ -1,13 +1,13 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { FlexPropsType } from '../shared/shared.types';
 import { TextWrapper } from './TextWrapper';
 
-type RenderPropArguments = { columnId: string };
-type RenderFunc = (props: RenderPropArguments) => ReactNode;
-type Children = ReactNode | RenderFunc;
+type RenderProp = {
+  columnId: string;
+};
 
 export type Props = {
-  children?: Children;
+  children?: React.ReactNode | ((obj: RenderProp) => React.ReactNode);
   /**
    * Define which column which cell it belongs to and sets the column layout defined in the `Header`
    */
@@ -16,8 +16,7 @@ export type Props = {
 
 export type CellComponents = { TextWrapper: typeof TextWrapper };
 
-export type CellComponent = React.FC<Props & React.ComponentProps<typeof TextWrapper>> &
-  CellComponents;
+export type CellComponent = React.FC<Props> & CellComponents;
 
 type ExpandCellProps = {
   /**
