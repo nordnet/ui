e6146cd133884c972893b188422db5b049a4f56d
Mansour Mansour <mansour@mjmonline.se>
2022-11-14 14:19:52 +0100

    fix: any prop

--- src/Atoms/Truncate/Truncate.types.ts
+++ src/Atoms/Truncate/Truncate.types.ts
@@ -1,7 +1,7 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 export type Props = {
   as?: any;
   className?: string;
   children: ReactNode;
-} & Omit<React.HTMLProps<HTMLElement>, 'as'>;
+};
