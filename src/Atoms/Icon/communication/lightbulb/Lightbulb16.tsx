66f7700547e02ecc668be9e8f33e15a0d224f803
karmal <karam.malkon@nordnet.se>
2022-11-16 10:23:11 +0100

    fix: 🐛 use correct width

--- src/Atoms/Icon/communication/lightbulb/Lightbulb16.tsx
+++ src/Atoms/Icon/communication/lightbulb/Lightbulb16.tsx
@@ -5,12 +5,12 @@ import { IconProps } from '../../IconBase.types';
 
 const Lightbulb16: React.FC<IconProps> = (props) => {
   return (
-    <IconBase {...props} width={12} height={16}>
+    <IconBase {...props} width={16} height={16}>
       <path
-        d="M2 6a4 4 0 1 1 5.6 3.668L7 9.93V13h2v-1.803a6 6 0 1 0-6 0V13h2V9.93l-.6-.262A4.001 4.001 0 0 1 2 6Z"
+        d="M4 6a4 4 0 1 1 5.6 3.668L9 9.93V13h2v-1.803a6 6 0 1 0-6 0V13h2V9.93l-.6-.262A4.001 4.001 0 0 1 4 6Z"
         fill="currentColor"
       />
-      <path d="M7 6a1 1 0 0 0-1-1V3a3 3 0 0 1 3 3H7ZM3 16v-2h6v2H3Z" fill="currentColor" />
+      <path d="M9 6a1 1 0 0 0-1-1V3a3 3 0 0 1 3 3H9ZM5 16v-2h6v2H5Z" fill="currentColor" />
     </IconBase>
   );
 };
