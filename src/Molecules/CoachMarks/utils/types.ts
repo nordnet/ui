1cff835ddcff3747eb0a60da2b91d58592b5a4f5
karmal <karam.malkon@nordnet.se>
2022-11-21 12:45:34 +0100

    fix: 🐛 allow passing custom px and py for each step

--- src/Molecules/CoachMarks/utils/types.ts
+++ src/Molecules/CoachMarks/utils/types.ts
@@ -1,1 +1,7 @@
-export type MakeBackdropPath = (rect: ClientRect, padding?: number, isCircular?: boolean) => string;
+export type MakeBackdropPath = (
+  rect: ClientRect,
+  padding?: number,
+  isCircular?: boolean,
+  px?: number | string,
+  py?: number | string,
+) => string;
