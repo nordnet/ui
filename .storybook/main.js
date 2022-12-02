f3beaface4452e6d9645cec80358cc51b6ee9419
Mansour Mansour <mansour.mansour@nordnet.se>
2022-12-02 20:53:44 +0100

    fix: relative paths to stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,5 @@
-const path = require('path');
-
-const srcPath = path.join(process.cwd(), 'src');
-const storiesDir = process.env.STORYBOOK_DIRECTORY
-  ? path.join(srcPath, process.env.STORYBOOK_DIRECTORY)
-  : srcPath;
-
 module.exports = {
-  stories: ['../docs/**/*.stories.mdx', path.join(storiesDir, '**/*.stories.@(js|jsx|ts|tsx|mdx)')],
+  stories: ['../docs/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-storysource',
