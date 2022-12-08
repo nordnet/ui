cbd1550e652c073f2dea1fe29ed5a76f8a2c489d
Killian de Buitleir <killian.debuitleir@gmail.com>
2022-12-08 14:19:34 +0100

    fix: revert to previous imports

--- src/Molecules/CoachMarks/CoachMarks.tsx
+++ src/Molecules/CoachMarks/CoachMarks.tsx
@@ -1,70 +1,24 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import React, { useCallback, useRef, useState } from 'react';
 import { usePopper } from 'react-popper';
 import FocusLock from 'react-focus-lock';
 import { Button, Flexbox, Icon, Typography } from '../..';
-import { ColsTrimmerProps, Component } from './CoachMarks.types';
+import { Component } from './CoachMarks.types';
 import { makeBackdropPath } from './utils';
 import { useOnClickOutside, useWindowSize, useSafeLayoutEffect } from '../../common/Hooks';
+import useScrollPosition from './hooks/useScrollPosition';
 import Bubble from './Bubble';
 import BubbleArrow from './BubbleArrow';
 import { OFFSET_AWAY_FROM_REFERENCE } from './Bubble/consts';
-import { BACKDROP_PADDING, CLOSE_ICON_SIZE } from './consts';
-
-const SVG = styled.svg`
-  overflow: hidden;
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 100vw;
-  height: 100vh;
-  fill: ${(p) => p.theme.color.modalBackdrop};
-  z-index: ${(p) => p.theme.zIndex.modal};
-  transition: all 0.3s ease-out, height 0s 0s, opacity 0.3s 0s;
-`;
-
-const TitleWrapper = styled.div<ColsTrimmerProps>`
-  padding-right: ${(p) => (p.$hasIcon ? 0 : p.theme.spacing.unit(CLOSE_ICON_SIZE))}px;
-`;
-
-const FooterFlex = styled(Flexbox)`
-  margin-top: auto;
-`;
-
-const IconFlex = styled(Flexbox)`
-  margin-bottom: ${(p) => p.theme.spacing.unit(1)}px;
-`;
-
-const NavigationButtonsContainer = styled(Flexbox)<{ $hasSingleButton?: boolean }>`
-  margin-left: auto;
-  min-width: ${(p) =>
-    p.$hasSingleButton ? `${p.theme.spacing.unit(25)}px` : `${p.theme.spacing.unit(50)}px`};
-`;
-
-const Content = styled.div`
-  color: ${(p) => p.theme.color.bubbleSecondaryText};
-`;
-
-const CloseButton = styled(Button)`
-  position: absolute;
-  top: ${(p) => p.theme.spacing.unit(5)}px;
-  right: ${(p) => p.theme.spacing.unit(5)}px;
-`;
-
-const useScrollPosition = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    const updatePosition = () => {
-      setScrollPosition(window.pageYOffset);
-    };
-    window.addEventListener('scroll', updatePosition);
-    updatePosition();
-    return () => window.removeEventListener('scroll', updatePosition);
-  }, []);
-
-  return scrollPosition;
-};
+import { BACKDROP_PADDING } from './consts';
+import {
+  CloseButton,
+  Content,
+  FooterFlex,
+  IconFlex,
+  NavigationButtonsContainer,
+  SVG,
+  TitleWrapper,
+} from './CoachMarks.styled';
 
 export const CoachMarks: Component = ({
   steps,
