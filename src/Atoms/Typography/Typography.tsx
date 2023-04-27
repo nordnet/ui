66564070f8bf8b1fd3ecd52caef3781f2eb45e80
karmal <karam.malkon@nordnet.se>
2023-04-27 15:02:56 +0200

    feat: 🎸 use colorTokens in Typography

--- src/Atoms/Typography/Typography.tsx
+++ src/Atoms/Typography/Typography.tsx
@@ -25,7 +25,7 @@ const getColor = (props: ThemedStyledProps<Props, Theme>) => {
     return 'inherit';
   }
 
-  return theme.color.text;
+  return theme.colorTokens.neutral.text_default;
 };
 
 export const TYPOGRAPHY_TYPES: Record<Types, Types> = {
