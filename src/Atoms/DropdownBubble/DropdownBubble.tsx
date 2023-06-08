c038e06652aaa458a390328469a66e1dd2ed1ad9
Jonna Kollin <jonna.kollin@nordnet.se>
2023-06-08 11:43:58 +0200

    fix: 🐛 add border radius as prop
    add border radius as prop to DropdownBubble since we use it on many
    components and it seems like it will be different. Default border radius
    is 4px

--- src/Atoms/DropdownBubble/DropdownBubble.tsx
+++ src/Atoms/DropdownBubble/DropdownBubble.tsx
@@ -133,7 +133,7 @@ export const DropdownBubble = styled.div<Props>`
       p.invertedColors ? p.theme.color.textLight : p.theme.color.text,
       p.textColor,
     )};
-  border-radius: ${({ theme }) => theme.borderRadius(4)};
+  border-radius: ${(p) => p.theme.borderRadius(p.borderRadius ? p.borderRadius : 4)};
   ${bottomAndTopPlacementCss}
   ${triangleCss}
 `;
