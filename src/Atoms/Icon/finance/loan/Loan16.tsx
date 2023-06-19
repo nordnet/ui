be2740f409041477a7320edf09aac0f012cd57ac
Garima Garima <gargar@v2xnp07kml.pilen.nordnet.se>
2023-06-19 10:20:26 +0200

    fix: update svg path

--- src/Atoms/Icon/finance/loan/Loan16.tsx
+++ src/Atoms/Icon/finance/loan/Loan16.tsx
@@ -7,11 +7,16 @@ const Loan16: React.FC<IconProps> = (props) => {
   return (
     <IconBase {...props} width={16} height={16}>
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M16 0H4V8H0V16H12V8H6V2H14V16H16V0ZM2 14V10H10V14H2Z 13C6.55228 13 7 12.5523 7 12C7 11.4477 6.55228 11 6 11C5.44772 11 5 11.4477 5 12C5 12.5523 5.44772 13 6 13Z"
+        d="M6 13C6.55228 13 7 12.5523 7 12C7 11.4477 6.55228 11 6 11C5.44772 11 5 11.4477 5 12C5 12.5523 5.44772 13 6 13Z"
         fill="currentColor"
       />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16 0H4V8H0V16H12V8H6V2H14V16H16V0ZM2 14V10H10V14H2Z"
+        fill="currentColor"
+      />
+      <path d="M13 5H10V3H13V5Z" fill="currentColor" />
     </IconBase>
   );
 };
