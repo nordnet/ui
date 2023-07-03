ca7e25201ae823e74b6462559110c77c928384bd
Mansour Mansour <mansour@mjmonline.se>
2023-07-03 13:31:38 +0200

    feat: add trackinId prop to button and link components

--- src/common/tracking.types.ts
+++ src/common/tracking.types.ts
@@ -1,5 +1,9 @@
 type TrackFunction = (componentName: string, e: React.SyntheticEvent, props: any) => void;
 
+export type TrackingProps = {
+  trackingId: string;
+};
+
 export interface TrackingContextData {
   track: TrackFunction;
 }
