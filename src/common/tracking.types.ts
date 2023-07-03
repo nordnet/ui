1e09d629c4ca063e6d3f0d54324229e636c8a7ba
Mansour J. Mansour <mansour.mansour@nordnet.se>
2023-07-03 15:02:26 +0200

    feat: add componentId prop to button and link components for analytics (#1372)
    Co-authored-by: John Lindahl <john@lindahl.tech>

--- src/common/tracking.types.ts
+++ src/common/tracking.types.ts
@@ -1,5 +1,9 @@
 type TrackFunction = (componentName: string, e: React.SyntheticEvent, props: any) => void;
 
+export type TrackingProps = {
+  componentId?: string;
+};
+
 export interface TrackingContextData {
   track: TrackFunction;
 }
