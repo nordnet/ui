c30480e86498777af9b02f93f1097e4aa39b5ac8
Karam Malkon <kmelkon@gmail.com>
2023-07-06 10:07:25 +0200

    feat: add global fill 12px icon

--- src/Atoms/Icon/finance/global/index.ts
+++ src/Atoms/Icon/finance/global/index.ts
@@ -1,8 +1,10 @@
+import Global12 from './Global12';
 import Global16 from './Global16';
 import Global24 from './Global24';
 import Global32 from './Global32';
 
 export default {
+  Global12,
   Global16,
   Global24,
   Global32,
