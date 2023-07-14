e0fae4e1d1c6e309e25a4e0b594e19848870223f
Jonna Kollin <jonna.kollin@nordnet.se>
2023-07-14 06:53:06 +0200

    fix: 🐛 fix missing replacement

--- src/Atoms/DropdownBubble/DropdownBubble.tsx
+++ src/Atoms/DropdownBubble/DropdownBubble.tsx
@@ -133,7 +133,7 @@ export const DropdownBubble = styled.div<Props>`
       p.invertedColors ? p.theme.color.textLight : p.theme.color.text,
       p.textColor,
     )};
-  border-radius: ${(p) => p.theme.borderRadius(p.borderRadius ? p.borderRadius : 4)};
+  border-radius: ${(p) => (p.borderRadius ? p.borderRadius : p.theme.borderRadius4())};
   ${bottomAndTopPlacementCss}
   ${triangleCss}
 `;
