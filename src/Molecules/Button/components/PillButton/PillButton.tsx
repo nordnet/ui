20726a11ebe5e2aeaa46e0fe2ea7737354361f8e
Gustav Pettersson <gustav.pettersson@nordnet.se>
2023-09-07 16:35:56 +0200

    fix: remove unsued import

--- src/Molecules/Button/components/PillButton/PillButton.tsx
+++ src/Molecules/Button/components/PillButton/PillButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Theme } from '../../../../theme/theme.types';
 
 import Button from '../BaseButton';
