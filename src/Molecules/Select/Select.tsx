653a3102f334b3cb1ab8e62915b3b1d0360f38cd
Gustav Pettersson <gustav.pettersson@nordnet.se>
2023-10-05 11:47:11 +0200

    fix: clean up todo comments

--- src/Molecules/Select/Select.tsx
+++ src/Molecules/Select/Select.tsx
@@ -53,25 +53,8 @@ export const Select = forwardRef<HTMLButtonElement, Props>(function SelectCompon
 
   const Trigger = slots.trigger || DefaultTrigger;
   // Needs to be done for a 1.0 release
-  // TODO: write tests, in forms as well
-  // TODO: Mobile. Should it have the handle (-) in the top?. Devider in the bottom, should it be indented? Should the title equal the label?
-  // TODO: With radio looking checkmarks?
-  // TODO: Dropdown should not close when clicking the search or the toggle all button in the header (maybe footer?).
-  // TODO: handle toggleAll
-  // TODO: decide if slots and slotsProps is preferable over props like valueDisplay and stick to one
-  // TODO: Check with Zoe if we can get figma design for the "old" sizes.
-  // TODO: Implement disabled design and make it accessible to screen readers
-  // TODO: Mobile design.
-  // TODO: Bottom sheet design.
-  // TODO: Dark mode story fixes and component color fixes
-  // TODO: Double check multiselect checkbox design
 
-  // 2.0
-  // TODO: Pill dropdown
-  // TODO: Badge dropdown
-  // TODO: Loading state?
-  // TODO: investigate if the wide select with columns is needed, see figma
-  // TODO: Do we want to use popper for the dropdown?
+  // TODO: Check with Zoe if we can get figma design for the "old" sizes.
 
   return (
     <>
