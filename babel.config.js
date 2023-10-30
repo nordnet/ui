99284d74410ef6a34fea6592ac1f0f1c77c22181
Mansour Mansour <mansour@mjmonline.se>
2023-10-30 15:30:05 +0100

    remove old babel packages that now is baked into babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -15,9 +15,6 @@ module.exports = (api) => {
   ];
 
   const plugins = [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     'babel-plugin-styled-components',
     'ramda',
     [
