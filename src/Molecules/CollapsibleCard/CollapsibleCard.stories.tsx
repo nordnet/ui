c3c2aebee15dccdaa9aaf888a9a148bb2b4c875b
Gustav Pettersson <gustav.pettersson@nordnet.se>
2023-11-02 16:11:00 +0100

    fix: CollapsibleCard story

--- src/Molecules/CollapsibleCard/CollapsibleCard.stories.tsx
+++ src/Molecules/CollapsibleCard/CollapsibleCard.stories.tsx
@@ -107,29 +107,31 @@ export const collapsibleWithOnClickListener = {
   name: 'Collapsible with onClick listener',
 };
 
+const CollapsibleWithoutIndicatorComponent = () => {
+  const [extraText, setExtraText] = useState(false);
+
+  const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.`;
+
+  return (
+    <>
+      <Button onClick={() => setExtraText(!extraText)}>Toggle collapse</Button>
+      <CollapsibleCard title="Collapsible without indicator" noIndicator>
+        <Typography type="primary" as={styledText}>
+          {text}
+          {extraText && text}
+        </Typography>
+      </CollapsibleCard>
+    </>
+  );
+};
+
 export const CollapsibleWithoutIndicator = {
-  render: () => {
-    const [extraText, setExtraText] = useState(false);
-
-    const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-            est laborum.`;
-
-    return (
-      <>
-        <Button onClick={() => setExtraText(!extraText)}>Toggle collapse</Button>
-        <CollapsibleCard title="Collapsible without indicator" noIndicator>
-          <Typography type="primary" as={styledText}>
-            {text}
-            {extraText && text}
-          </Typography>
-        </CollapsibleCard>
-      </>
-    );
-  },
+  render: () => <CollapsibleWithoutIndicatorComponent />,
 
   name: 'Collapsible without indicator',
 };
