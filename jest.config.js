8c88416f3fdc288674e0676a26a20e9ce913aaec
Mansour Mansour <mansour@mjmonline.se>
2023-11-20 15:51:48 +0100

    enable storyshots

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,14 @@ module.exports = {
   setupFiles: ['./jest.setup.js'],
   testPathIgnorePatterns: ['/dist/', '/node_modules/'],
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'md'],
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'md', 'mdx'],
   transformIgnorePatterns: ['node_modules/(?!(@statecharts|react-syntax-highlighter)/.*)'],
   moduleNameMapper: {
     '\\.(mdx?)$': '<rootDir>/src/mdMock.ts',
     '\\.(jpg|png|svg)$': '<rootDir>/__mocks__/fileMock.js',
-    'react-markdown': '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx',
   },
 };
