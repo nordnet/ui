bd1799fe042999cbb148cbe21393b948309fcb45
Jesper Winér <jesper.winer@nordnet.se>
2024-01-02 13:46:37 +0100

    fix: rework backdrop a little to make it work above position static elements

--- src/Molecules/BottomSheet/BottomSheet.types.ts
+++ src/Molecules/BottomSheet/BottomSheet.types.ts
@@ -1,10 +1,5 @@
 import React from 'react';
 
-export type BackdropWrapperProps = {
-  children?: React.ReactNode;
-  showBackdrop: boolean;
-};
-
 export type Props = {
   children?: React.ReactNode;
   className?: string;
