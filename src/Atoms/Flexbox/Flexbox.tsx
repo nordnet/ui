856c34520df7c3193e8c841e574a0bfbdb6dcac2
Michal Maciej Dzienis <dzienis.michal.maciej@gmail.com>
2024-01-12 15:43:02 +0100

    fix: the not:empty css query messes up how flex box are applies … (#1572)
    * refactor: the not:empty css query messes up how flex box are applies to children and grandchildren when styled-components version is updated to 16.0.0 and ahead
    
    * fix: removed extra ';'

--- src/Atoms/Flexbox/Flexbox.tsx
+++ src/Atoms/Flexbox/Flexbox.tsx
@@ -72,17 +72,14 @@ const getGapStyles = (theme: Theme, gap: Props['gap']) => {
   }
 
   if (typeof gap === 'number') {
-    gapStyle = `${theme.spacing.unit(gap)}px;`;
+    gapStyle = `${theme.spacing.unit(gap)}px`;
   } else if (typeof gap === 'string') {
     gapStyle = gap;
   } else {
-    gapStyle = `${theme.spacing.unit(gap.row)}px ${theme.spacing.unit(gap.column)}px;`;
+    gapStyle = `${theme.spacing.unit(gap.row)}px ${theme.spacing.unit(gap.column)}px`;
   }
-  /*
-    :not(:empty) to fix issue with printing in chrome
-    https://bugs.chromium.org/p/chromium/issues/detail?id=1161709
-  */
-  return `:not(:empty) { gap: ${gapStyle}; }`;
+
+  return `gap: ${gapStyle};`;
 };
 
 const getContainerStyles = (p: Props & { theme: Theme }) => `
