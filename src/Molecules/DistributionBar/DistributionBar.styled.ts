e0ee95d9bad6080fefad22eb522e31485122484a
Rebecca Hansson Klinc <rebecca.hansson.klinc@nordnet.se>
2024-01-12 07:36:56 +0100

    Added border to distribution bar

--- src/Molecules/DistributionBar/DistributionBar.styled.ts
+++ src/Molecules/DistributionBar/DistributionBar.styled.ts
@@ -20,7 +20,7 @@ export const Bar = styled.div<{ $hoverEffect?: boolean }>`
   top: 0;
   left: 0;
   height: 100%;
-  background-color: ${(p) => p.theme.color.tableEmphasis};
+  background-color: ${(p) => p.theme.colorTokens.action.background_weak};
   z-index: -1;
   margin: 0 ${(p) => p.theme.spacing.unit(4)}px;
 
@@ -30,7 +30,7 @@ export const Bar = styled.div<{ $hoverEffect?: boolean }>`
     content: '';
     display: block;
     border-radius: 100px 0 0 100px;
-    background-color: ${(p) => p.theme.color.tableEmphasis};
+    background-color: ${(p) => p.theme.colorTokens.action.background_weak};
     height: 100%;
     width: ${(p) => p.theme.spacing.unit(4)}px;
     top: 0;
@@ -44,13 +44,17 @@ export const Bar = styled.div<{ $hoverEffect?: boolean }>`
   }
 
   ${StyledFlexBox}:hover & {
-    border: 1px solid black;
-    border-radius: 0px;
+    border: 1px solid ${({ theme, $hoverEffect }) =>
+          $hoverEffect ? theme.colorTokens.action.border_weak : 'transparent'};
     &::before {
-      border-left: 1px solid black;
+      box-shadow: -1px 0px 0px 1px
+        ${({ theme, $hoverEffect }) =>
+          $hoverEffect ? theme.colorTokens.action.border_weak : 'transparent'};
     }
     &::after {
-      border-right: 1px solid black;
+      box-shadow: 1px 0px 0px 1px
+        ${({ theme, $hoverEffect }) =>
+          $hoverEffect ? theme.colorTokens.action.border_weak : 'transparent'};
     }
   }
 `;
