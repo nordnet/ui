fd0ae8c1795008db7bd57439e160c42a474ae192
Mansour Mansour <mansour@mjmonline.se>
2024-03-14 23:15:04 +0100

    simplify formfield

--- src/Molecules/FormField/FormField.tsx
+++ src/Molecules/FormField/FormField.tsx
@@ -1,43 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
-import { AnimatePresence, motion } from 'framer-motion';
+
 import { Props } from './FormField.types';
-import { Fieldset, FormLabel, Legend, Typography, VisuallyHidden } from '../..';
+import { FormGroup, FormLabel, VisuallyHidden } from '../..';
+import { After } from './After';
 import { Tooltip } from './Tooltip';
 
-const Wrapper = styled.div<{ $width?: string | number }>`
+const Root = styled.div<{ $width?: string | number }>`
   ${(p) => (p.$width ? `width: ${p.$width};` : 'width: 200px;')}
   max-width: 100%;
   display: inline-block;
 `;
 
-const BottomWrapper = styled(motion.div)``;
+export const FormField = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const {
+    children,
+    className,
+    error,
+    extraInfo,
+    fieldId,
+    group,
+    hideLabel,
+    label,
+    labelTooltip,
+    labelTooltipPosition,
+    labelTooltipInModal,
+    required = false,
+    width,
+    disabled,
+  } = props;
+  const labelText = label && `${label}${required ? ' *' : ''}`;
+  const componentWidth = typeof width === 'number' ? `${width}px` : width;
 
-export const FormField = React.forwardRef<HTMLDivElement, Props>(
-  (
-    {
-      children,
-      className,
-      error,
-      extraInfo,
-      fieldId,
-      group,
-      hideLabel,
-      label,
-      labelTooltip,
-      labelTooltipPosition,
-      labelTooltipInModal,
-      required = false,
-      width,
-      disabled,
-    },
-    ref,
-  ) => {
-    const labelText = label && `${label}${required ? ' *' : ''}`;
+  if (group) {
+    return <FormGroup {...props} />;
+  }
 
-    return (
-      <Wrapper $width={width} className={className} ref={ref}>
-        {!group && !fieldId && (
+  return (
+    <Root className={className} $width={componentWidth} ref={ref}>
+      {fieldId ? (
+        <>
+          <FormLabel hideLabel={hideLabel} forId={fieldId} disabled={disabled}>
+            <Tooltip
+              labelTooltip={hideLabel ? '' : labelTooltip}
+              labelTooltipInModal={labelTooltipInModal}
+              labelTooltipPosition={labelTooltipPosition}
+            >
+              {hideLabel ? <VisuallyHidden>{labelText}</VisuallyHidden> : labelText}
+            </Tooltip>
+          </FormLabel>
+          {children}
+        </>
+      ) : (
+        <>
           <FormLabel disabled={disabled}>
             <Tooltip
               labelTooltip={hideLabel ? '' : labelTooltip}
@@ -48,64 +63,9 @@ export const FormField = React.forwardRef<HTMLDivElement, Props>(
             </Tooltip>
             {children}
           </FormLabel>
-        )}
-        {!group && fieldId && (
-          <>
-            <FormLabel hideLabel={hideLabel} forId={fieldId} disabled={disabled}>
-              <Tooltip
-                labelTooltip={hideLabel ? '' : labelTooltip}
-                labelTooltipInModal={labelTooltipInModal}
-                labelTooltipPosition={labelTooltipPosition}
-              >
-                {hideLabel ? <VisuallyHidden>{labelText}</VisuallyHidden> : labelText}
-              </Tooltip>
-            </FormLabel>
-            {children}
-          </>
-        )}
-        {group && (
-          <Fieldset>
-            <Tooltip
-              labelTooltip={hideLabel ? '' : labelTooltip}
-              labelTooltipInModal={labelTooltipInModal}
-              labelTooltipPosition={labelTooltipPosition}
-            >
-              {hideLabel ? (
-                <VisuallyHidden>
-                  <Legend styleType="label">{label}</Legend>
-                </VisuallyHidden>
-              ) : (
-                <Legend styleType="label">{label}</Legend>
-              )}
-            </Tooltip>
-            {children}
-          </Fieldset>
-        )}
-        <AnimatePresence>
-          {(error || extraInfo) && (
-            <BottomWrapper
-              initial={{ y: -10, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: 0, opacity: 0 }}
-              aria-live="polite"
-            >
-              {error ? (
-                <Typography type="tertiary" color={(t) => t.color.danger}>
-                  {error}
-                </Typography>
-              ) : null}
-              {extraInfo ? (
-                <Typography
-                  type="tertiary"
-                  color={(t) => (disabled ? t.color.disabled : t.color.label)}
-                >
-                  {extraInfo}
-                </Typography>
-              ) : null}
-            </BottomWrapper>
-          )}
-        </AnimatePresence>
-      </Wrapper>
-    );
-  },
-);
+        </>
+      )}
+      <After error={error} extraInfo={extraInfo} disabled={disabled} />
+    </Root>
+  );
+});
--- src/Molecules/FormField/FormField.types.ts
+++ src/Molecules/FormField/FormField.types.ts
@@ -1,19 +1,19 @@
 import { Props as TooltipProps } from './Tooltip/Tooltip.types';
 
 export type Props = {
-  /** Label should always be presented - A11y */
-  label: string;
-  /** But you can hide it visually */
-  hideLabel?: boolean;
   children: React.ReactNode;
   className?: string;
+  disabled?: boolean;
   error?: string;
   extraInfo?: string;
   fieldId?: string;
   group?: boolean;
+  /** Label can be visually hidden */
+  hideLabel?: boolean;
+  /** Label should always be presented - A11y */
+  label: string;
   ref?: React.Ref<HTMLDivElement>;
   required?: boolean;
-  disabled?: boolean;
 
   /**
    * You need to specify width
